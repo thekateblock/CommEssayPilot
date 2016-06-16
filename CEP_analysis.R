76b3b07912ce586ab56fe938525a807a5341b519
thekateblock <kblock@psych.ubc.ca>
2016-06-16 13:24:59 -0700

    First commit for first round of analyses for communal essay pilot

---
+++ CEP_analysis.R
@@ -0,0 +1,239 @@
+
+#'use dataset.attn for those who have have done the essay and stayen on topic
+#' use dataset.attn2 for those who have done essay and passed attention check
+
+#correlations between main outcomes
+rcorr(cbind(dataset.attn$comm_imp, dataset.attn$comm_sat, dataset.attn$comm_Ben, dataset.attn$agen_imp, dataset.attn$agen_sat, dataset.attn$agen_Ben))
+
+#' ANOVA's for Mean differences in communal importance
+comm_impAN <- aov(comm_imp ~ Condition_TXT, data=dataset.attn)
+summary (comm_impAN)
+TukeyHSD(comm_impAN)
+
+library(gplots)
+Condition_TXT <- factor(dataset.attn$Condition_TXT)
+plotmeans(dataset.attn$comm_imp~Condition_TXT,xlab="Condition",
+          ylab= "Importance of Communal Goals", main="Importance of communal Values\n BY condition")
+
+# ANOVA's for Mean differences in agentic importance
+agen_impAN <- aov(agen_imp ~ Condition_TXT, data=dataset.attn)
+summary (agen_impAN)
+TukeyHSD(agen_impAN)
+
+# ANOVA's for Mean differences in communal satisfaction
+comm_satAN <- aov(comm_sat ~ Condition_TXT, data=dataset.attn)
+summary (comm_satAN)
+TukeyHSD(comm_satAN)
+
+library(gplots)
+Condition_TXT <- factor(dataset.attn$Condition_TXT)
+plotmeans(dataset.attn$comm_sat~Condition_TXT,xlab="Condition",
+          ylab= "Satisfaction of Communal Goals", main="Satisfaction of communal Goals\n BY condition")
+
+# ANOVA's for Mean differences in agentic statisfaction
+# ANOVA's for Mean differences in communal satisfaction
+agen_satAN <- aov(agen_Sat ~ Condition_TXT, data=dataset.attn)
+summary (agen_satAN)
+TukeyHSD(agen_satAN)
+
+# ANOVA's for Mean differences in communal benefit
+comm_benAN <- aov(comm_Ben ~ Condition_TXT, data=dataset.attn)
+summary (comm_benAN)
+TukeyHSD(comm_benAN)
+
+library(gplots)
+Condition_TXT <- factor(dataset.attn$Condition_TXT)
+plotmeans(dataset.attn$comm_Ben~Condition_TXT,xlab="Condition",
+          ylab= "Benefit of Communal Goals", main="Benefit of communal Goals\n BY condition")
+
+# ANOVA's for Mean differences in agentic benefit
+
+agen_benAN <- aov(agen_Ben ~ Condition_TXT, data=dataset.attn)
+summary (agen_benAN )
+TukeyHSD(agen_benAN )
+
+# ANOVA's for Mean differences in task ratings.
+diffAN <- aov(Difficulty_Write ~ Condition_TXT, data=dataset.attn)
+summary (diffAN)
+TukeyHSD(diffAN)
+
+wellAN <- aov(Well_Written ~ Condition_TXT, data=dataset.attn)
+summary (wellAN)
+TukeyHSD(wellAN)
+
+engageAN <- aov(dataset.attn$Engaging ~ Condition_TXT, data=dataset.attn)
+summary (engageAN)
+TukeyHSD(engageAN)
+
+plotmeans(dataset.attn$Engaging~Condition_TXT,xlab="Condition",
+          ylab= "engaging", main="Engagement \n BY condition")
+
+understandAN <- aov(dataset.attn$Understand ~ Condition_TXT, data=dataset.attn)
+summary (understandAN)
+TukeyHSD(understandAN)
+
+surprisedAN <- aov(dataset.attn$Surprised ~ Condition_TXT, data=dataset.attn)
+summary (surprisedAN)
+TukeyHSD(surprisedAN)
+
+plotmeans(dataset.attn$Surprised~Condition_TXT,xlab="Condition",
+          ylab= "Surprise", main="Surprised by article \n BY condition")
+
+######## Do same test with the larger sample that also included those who wrote counter the condition
+###########################################
+
+#' ANOVA's for Mean differences in communal importance
+comm_impAN <- aov(comm_imp ~ Condition_TXT, data=dataset.attn3)
+summary (comm_impAN)
+TukeyHSD(comm_impAN)
+
+library(gplots)
+Condition_TXT <- factor(dataset.attn$Condition_TXT)
+plotmeans(dataset.attn$comm_imp~Condition_TXT,xlab="Condition",
+          ylab= "Importance of Communal Goals", main="Importance of communal Values\n BY condition")
+
+# ANOVA's for Mean differences in agentic importance
+agen_impAN <- aov(agen_imp ~ Condition_TXT, data=dataset.attn3)
+summary (agen_impAN)
+TukeyHSD(agen_impAN)
+
+# ANOVA's for Mean differences in communal satisfaction
+comm_satAN <- aov(comm_sat ~ Condition_TXT, data=dataset.attn3)
+summary (comm_satAN)
+TukeyHSD(comm_satAN)
+
+
+Condition_TXT <- factor(dataset.attn3$Condition_TXT)
+plotmeans(dataset.attn3$comm_sat~Condition_TXT,xlab="Condition",
+          ylab= "Satisfaction of Communal Goals", main="Satisfaction of communal Goals\n BY condition")
+
+# ANOVA's for Mean differences in agentic statisfaction
+agen_satAN <- aov(agen_Sat ~ Condition_TXT, data=dataset.attn3)
+summary (agen_satAN)
+TukeyHSD(agen_satAN)
+
+# ANOVA's for Mean differences in communal benefit
+comm_benAN <- aov(comm_Ben ~ Condition_TXT, data=dataset.attn3)
+summary (comm_benAN)
+TukeyHSD(comm_benAN)
+
+library(gplots)
+Condition_TXT <- factor(dataset.attn$Condition_TXT)
+plotmeans(dataset.attn$comm_Ben~Condition_TXT,xlab="Condition",
+          ylab= "Benefit of Communal Goals", main="Benefit of communal Goals\n BY condition")
+
+# ANOVA's for Mean differences in agentic benefit
+
+agen_benAN <- aov(agen_Ben ~ Condition_TXT, data=dataset.attn3)
+summary (agen_benAN )
+TukeyHSD(agen_benAN )
+
+# ANOVA's for Mean differences in task ratings.
+diffAN <- aov(Difficulty_Write ~ Condition_TXT, data=dataset.attn3)
+summary (diffAN)
+TukeyHSD(diffAN)
+
+plotmeans(dataset.attn3$Difficulty_Write~Condition_TXT,xlab="Condition",
+          ylab= "difficult", main="Difficulty writing \n BY condition")
+
+wellAN <- aov(Well_Written ~ Condition_TXT, data=dataset.attn3)
+summary (wellAN)
+TukeyHSD(wellAN)
+
+engageAN <- aov(dataset.attn$Engaging ~ Condition_TXT, data=dataset.attn3)
+summary (engageAN)
+TukeyHSD(engageAN)
+
+plotmeans(dataset.attn$Engaging~Condition_TXT,xlab="Condition",
+          ylab= "engaging", main="Engagement \n BY condition")
+
+understandAN <- aov(dataset.attn$Understand ~ Condition_TXT, data=dataset.attn3)
+summary (understandAN)
+TukeyHSD(understandAN)
+
+surprisedAN <- aov(dataset.attn$Surprised ~ Condition_TXT, data=dataset.attn3)
+summary (surprisedAN)
+TukeyHSD(surprisedAN)
+
+plotmeans(dataset.attn3$Surprised~Condition_TXT,xlab="Condition",
+          ylab= "Surprise", main="Surprised by article \n BY condition")
+
+#### excluding those people who guessed the hypothesis
+####################################################################
+
+#' ANOVA's for Mean differences in communal importance
+comm_impAN <- aov(comm_imp ~ Condition_TXT, data=dataset.attn5)
+summary (comm_impAN)
+TukeyHSD(comm_impAN)
+
+library(gplots)
+Condition_TXT <- factor(dataset.attn5$Condition_TXT)
+plotmeans(dataset.attn5$comm_imp~Condition_TXT,xlab="Condition",
+          ylab= "Importance of Communal Goals", main="Importance of communal Values\n BY condition")
+
+# ANOVA's for Mean differences in agentic importance
+agen_impAN <- aov(agen_imp ~ Condition_TXT, data=dataset.attn5)
+summary (agen_impAN)
+TukeyHSD(agen_impAN)
+
+# ANOVA's for Mean differences in communal satisfaction
+comm_satAN <- aov(comm_sat ~ Condition_TXT, data=dataset.attn5)
+summary (comm_satAN)
+TukeyHSD(comm_satAN)
+
+
+Condition_TXT <- factor(dataset.attn5$Condition_TXT)
+plotmeans(dataset.attn5$comm_Sat~Condition_TXT,xlab="Condition",
+          ylab= "Satisfaction of Communal Goals", main="Satisfaction of communal Goals\n BY condition")
+
+# ANOVA's for Mean differences in agentic statisfaction
+agen_satAN <- aov(agen_Sat ~ Condition_TXT, data=dataset.attn5)
+summary (agen_satAN)
+TukeyHSD(agen_satAN)
+
+# ANOVA's for Mean differences in communal benefit
+comm_benAN <- aov(comm_Ben ~ Condition_TXT, data=dataset.attn5)
+summary (comm_benAN)
+TukeyHSD(comm_benAN)
+
+library(gplots)
+Condition_TXT <- factor(dataset.attn5$Condition_TXT)
+plotmeans(dataset.attn5$comm_Ben~Condition_TXT,xlab="Condition",
+          ylab= "Benefit of Communal Goals", main="Benefit of communal Goals\n BY condition")
+
+# ANOVA's for Mean differences in agentic benefit
+
+agen_benAN <- aov(agen_Ben ~ Condition_TXT, data=dataset.attn5)
+summary (agen_benAN )
+TukeyHSD(agen_benAN )
+
+# ANOVA's for Mean differences in task ratings.
+diffAN <- aov(Difficulty_Write ~ Condition_TXT, data=dataset.attn5)
+summary (diffAN)
+TukeyHSD(diffAN)
+
+plotmeans(dataset.attn5$Difficulty_Write~Condition_TXT,xlab="Condition",
+          ylab= "difficult", main="Difficulty writing \n BY condition")
+
+wellAN <- aov(Well_Written ~ Condition_TXT, data=dataset.attn5)
+summary (wellAN)
+TukeyHSD(wellAN)
+
+engageAN <- aov(dataset.attn5$Engaging ~ Condition_TXT, data=dataset.attn5)
+summary (engageAN)
+TukeyHSD(engageAN)
+
+plotmeans(dataset.attn5$Engaging~Condition_TXT,xlab="Condition",
+          ylab= "engaging", main="Engagement \n BY condition")
+
+understandAN <- aov(dataset.attn5$Understand ~ Condition_TXT, data=dataset.attn5)
+summary (understandAN)
+TukeyHSD(understandAN)
+
+surprisedAN <- aov(dataset.attn5$Surprised ~ Condition_TXT, data=dataset.attn5)
+summary (surprisedAN)
+TukeyHSD(surprisedAN)
+
+plotmeans(dataset.attn5$Surprised~Condition_TXT,xlab="Condition",
+          ylab= "Surprise", main="Surprised by article \n BY condition")
+
---
+++ CEP_cleaning.R
@@ -0,0 +1,51 @@
+# reading in the data here
+
+dataset <- read.xlsx("Relating_to_Research_DATA_CLEAN.xlsx", sheetIndex = 1)
+
+#Let's you check that the variables are really the type you want them to be.
+str(dataset)
+
+count (dataset,Essay_Length)
+count(dataset, Essay_Content)
+count (dataset, English)
+count (dataset, Whatabout_Coded)
+hist(dataset$English, main="Historgram of Importance of communal goals", xlab="Importance 1-9")
+
+attach(dataset)
+mytable <- table(dataset$Condition_TXT,Essay_Content) 
+mytable
+
+attach(dataset)
+mytable2 <- table(dataset$Condition_TXT,Whatabout_Coded) 
+mytable2
+
+#give labels to gender data
+dataset$GenderF <- factor(dataset$Gender,
+                    levels = c(0,1),
+                    labels = c("Male", "Female"))
+
+## Making composite variables
+# composite for the personal importance of achieaving goals
+dataset$comm_imp <- ((dataset$Imp_comm1 + dataset$Imp_comm2 + dataset$Imp_comm3 + dataset$Imp_comm4 + dataset$Imp_comm5 + dataset$Imp_comm6 + dataset$Imp_comm7)/7)
+dataset$agen_imp <- ((dataset$Imp_agen1 + dataset$Imp_agen2 + dataset$Imp_agen3 + dataset$Imp_agen4 + dataset$Imp_agen5 + dataset$Imp_agen6 + dataset$Imp_agen7)/7)
+
+
+hist(dataset.attn$comm_imp, main="Historgram of Importance of communal goals", xlab="Importance 1-9")
+
+# composite for the personal satisfaction of achieaving goals
+dataset$comm_Sat <- ((dataset$Sat_comm1 + dataset$Sat_comm2 + dataset$Sat_comm3 + dataset$Sat_comm4 + dataset$Sat_comm5 + dataset$Sat_comm6 + dataset$Sat_comm7)/7)
+dataset$agen_Sat <- ((dataset$Sat_agen1 + dataset$Sat_agen2 + dataset$Sat_agen3 + dataset$Sat_agen4 + dataset$Sat_agen5 + dataset$Sat_agen6 + dataset$Sat_agen7)/7)
+
+# composite for how beneficial in the longterm achieving goals will be
+
+dataset$comm_Ben <- ((dataset$Ben_comm1 + dataset$Ben_comm2 + dataset$Ben_comm3 + dataset$Ben_comm4 + dataset$Ben_comm5 + dataset$Ben_comm6 + dataset$Ben_comm7)/7)
+dataset$agen_Ben <- ((dataset$Ben_agen1 + dataset$Ben_agen2 + dataset$Ben_agen3 + dataset$Ben_agen4 + dataset$Ben_agen5 + dataset$Ben_agen6 + dataset$Ben_agen7)/7)
+
+# chronbachs alpha's
+
+## excluding thos who did not write essay and who did not stay on TOPIC
+dataset.attn <- filter(dataset, Essay_Length!= "blank" & Essay_Content == 'IN LINE')
+dataset.attn2 <- filter(dataset, Essay_Length!= "blank" & Atn_Check1 == 2)
+dataset.attn3 <- filter(dataset, Essay_Length!= "blank")
+dataset.attn4 <- filter (dataset, Essay_Length!= "blank" & English < 13)
+dataset.attn5 <- filter (dataset, Essay_Length!= "blank" & Whatabout_Coded != 4)
